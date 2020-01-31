cacbf9886ccf161fbd7c2224be5e46d036f2dea6
Shubham Mishra <58596385+shubhammxv@users.noreply.github.com>
2020-01-31 11:04:53 +0530

    Add files via upload

--- autoannotate.py
+++ autoannotate.py
@@ -1,4 +1,13 @@
-# import the necessary packages
+# USAGE
+
+"""
+Load the CSV File of the Video IDs and run the script by following Command in the Terminal
+
+python (filename).py --tracker csrt
+
+"""
+
+# Import the necessary packages
 import pandas as pd
 import argparse
 import imutils
@@ -10,13 +19,15 @@ from shutil import copyfile
 # Default Tracking Video Number for first time Product
 version = 'v1'
 
-#CSV File of all the Session IDs to fetch Video Link
-videoIDs = pd.read_csv('CSV_FILE_PATH')
-videoNumber = 175       #Starting Session ID of the Video
+# CSV File of all the Session IDs to fetch Video Link
+csvPath = '/Users/shubham/Desktop/Niflr/Annotation-for-object-detectionin-videos-through-object-tracking/CSV Files/Eggs.csv'
+print(csvPath)
+videoIDs = pd.read_csv(csvPath)
+videoNumber = 0				   										# Initial Video Number in the file to Start From
 
 
-while (videoNumber != 200):      #Looping till Session ID in the CSV File
-	print("Opening New Video Stream:", videoNumber)
+while (videoNumber != 50):      					   					# Please Choose No. of Videos to Play in one loop as per convenience
+	print("Opening New Video Stream:", videoNumber)    				# Track of the Current Video Number
 	time.sleep(1)
 	ap = argparse.ArgumentParser()
 
@@ -24,47 +35,41 @@ while (videoNumber != 200):      #Looping till Session ID in the CSV File
 		help="OpenCV object tracker type")
 
 	args = vars(ap.parse_args())
-	# initialize a dictionary that maps strings to their corresponding
+	# Initialize a dictionary that maps strings to their corresponding
 	# OpenCV object tracker implementations
 	OPENCV_OBJECT_TRACKERS = {
 		"csrt": cv2.TrackerCSRT_create,
 		"kcf": cv2.TrackerKCF_create,
 	}
 
-	# initialize OpenCV's special multi-object tracker
+	#Initialize OpenCV's special multi-object tracker
 	trackers = cv2.MultiTracker_create()
-
 	count = 0
 	frameTaken = 0
 
 	videoID = videoIDs.iloc[videoNumber].sessionId
-	videoLink = 'API_OF_VIDEO' + videoID + 'Extension'
+	videoLink = 'https://storage.googleapis.com/live-stream-niflr-prod/' + videoID + '/cam1.mp4'
 	videoStream = cv2.VideoCapture(videoLink)
 	print("Grabbed New Streaming Video")
 	print("\n")
 
 	while True:
-		# grab the current frame, then handle if we are using a
-		# VideoStream or VideoCapture object
 		frame = videoStream.read()
 		frame = frame[1]
-		
 
-		# check to see if we have reached the end of the stream
+		# Check to see if we have reached the end of the stream
 		if frame is None:
 			videoNumber+=1
 			print("Closing Current Video and Opening New Video Stream:", videoNumber)
 			break
 
-		# resize the frame (so we can process it faster)
-		# frame = imutils.resize(frame, width=600)
+		# Resize the frame (so we can process it faster)
+		# Frame = imutils.resize(frame, width=600)
 
-		# grab the updated bounding box coordinates (if any) for each
-		# object that is being tracked
+		# Grab the updated bounding box coordinates (if any) for each object that is being tracked
 		(success, boxes) = trackers.update(frame)
 
-
-		# loop over the bounding boxes and draw then on the frame
+		# Loop over the bounding boxes and draw them on the frame
 		for box in boxes:
 			(x, y, w, h) = [int(v) for v in box]
 
@@ -86,28 +91,25 @@ while (videoNumber != 200):      #Looping till Session ID in the CSV File
 				frameTaken = frameTaken + 1
 			cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 		count = count+1
-		
-		cv2.imshow("Frame", frame)
-		keyStroke = cv2.waitKey(30) & 0xFF
 
-		# if the 's' key is selected, we are going to "select" a bounding
-		# box to track
+		cv2.imshow("Frame", frame)
+		keyStroke = cv2.waitKey(60) & 0xFF
+
+		# If the 's' key is selected, we are going to "select" a bounding box to track
 		if keyStroke == ord("s"):
-			# select the bounding box of the object we want to track (make
-			# sure you press ENTER or SPACE after selecting the ROI)
+			# Select the bounding box of the object we want to track (make
+			# Sure you press ENTER or SPACE after selecting the ROI)
 
 			box = cv2.selectROI("Frame", frame, fromCenter=False,
 				showCrosshair=True)
-			
+
 			tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
 			trackers.add(tracker, frame, box)
 			print("\n")
 			trackedProduct = input("Enter Tracked Product Name: ")
-			directory = "DIRECTORY_OF_ANNOTATION"+trackedProduct
+			directory = "/Users/shubham/Desktop/Niflr/Annotation-for-object-detectionin-videos-through-object-tracking/items/"+trackedProduct
 			classes = trackedProduct
 			print("\n")
-			
-			#To Create a New Folder for Product based on Input if already Tracked Previously
 			flagg = input("Previously Tracked Product? ")
 			if flagg == "n" or flagg == "no" or flagg == "N" or flagg == "NO":
 				os.mkdir(directory)
@@ -117,21 +119,21 @@ while (videoNumber != 200):      #Looping till Session ID in the CSV File
 				videoVersion = input("Video(Version) Number of Being Tracked Product: ")
 				version = videoVersion
 
-		# if the `q` key was pressed, break from the loop
-		elif (keyStroke == ord("q") or frameTaken == 5):
+		# If the `q` key was pressed, break from the loop
+		elif (keyStroke == ord("q") or frameTaken == 10):
 			print("frameTaken = ", frameTaken)
 			videoNumber+=1
 			print("\n")
 			videoStream.release()
 			break
-		#To Restart the Same Video
+
 		if (keyStroke == ord("r")):
 			videoStream = cv2.VideoCapture(videoLink)
 			print("Restarting the Same Video")
 			continue
-			
-#To release File Pointer
+
+# Release the file pointer
 videoStream.release()
 
-# close all windows
+# Close all windows
 cv2.destroyAllWindows()
